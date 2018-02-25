11ca992c14df4bd6e06ab6de9b22fc4c68b57fb0
StephenDK <stephendklein08@gmail.com>
2018-02-25 10:16:48 -0800

    File tree and server setup

---
+++ public/js/blog.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    // blogContainer vairable to hold all posts
+    var blogContainer = $(".blog-container");
+    // variable to hold the category
+    var postCategorySelect = $("#category");
+
+    // click events for the edit and delete buttons
+    
+
+
+
+    // function that grabs postd from the databse 
+    function getPosts(category) {
+        var 
+    }
+})
---
+++ server.js
@@ -0,0 +1,46 @@
+import { listenerCount } from "cluster";
+
+// Starting point for node/express server 
+
+// Dependencies
+// *****************************************************
+// 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Setup for the express application
+// *****************************************************
+// 
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Require for database models here
+// *****************************************************
+
+
+// Setup express application to handle data parsing middleware
+// *****************************************************
+// 
+
+app.use(bodyParser.urlencoded({extended: false }));
+// parse applicaton/json
+app.use(bodyParser.json());
+
+// Static directory when users visit
+app.use(express.static("public"));
+
+// Routes
+// *****************************************************
+// 
+// API Routes
+
+// HTML Routes
+
+// Sync database and start the express application
+// *****************************************************
+db.sequelize.sync({}).then(function() {
+    app.listen(PORT, function() {
+        console.log("Application listening on PORT " + PORT);
+    });
+});
+
